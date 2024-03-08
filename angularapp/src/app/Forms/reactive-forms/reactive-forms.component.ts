d92e08d09ae28b7bb4fd62b1dea80e5014d1f903
Suraj Bhosale <bhosalesuraj074@gmail.com>
2024-03-08 09:18:41 +0530

    custom validation in reactive form

--- angularapp/src/app/Forms/reactive-forms/reactive-forms.component.ts
+++ angularapp/src/app/Forms/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -14,16 +14,34 @@ export class ReactiveFormsComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    // this.myReactiveForm.setValue({
+    //   'email': 'jalndhar@gmail.com',
+    //   'password': '1234567'
+    // });
+
+    this.myReactiveForm.patchValue({
+      'email': 'virbhdra@gmail.com'
+    })
   }
   createReactiveForm() {
 
     this.myReactiveForm = new FormGroup({
-      'email': new FormControl('', [Validators.required, Validators.email]),
+      'email': new FormControl('',[Validators.required, Validators.minLength(7), Validators.maxLength(50), this.EmailNotAllowed]),
       'password': new FormControl('', [Validators.required])
     })
   }
   onSubmit(){
     console.log(this.myReactiveForm);
   }
+   
+  // custom validator function 
 
+  EmailNotAllowed(control: FormControl) : ValidationErrors | null {
+    const emailNotAllowed = ['codemind@gmail.com', 'jack@gmail.com'];
+    const value = control.value as string;
+    if (emailNotAllowed.indexOf(value) !== -1) {
+      return {'emailNotAllowed': true};
+    }
+    return null
+  }
 }
