7d27e62ed6b307d65b2192e18a1971fab455abfb
Suraj Bhosale <125651189+bhosalesuraj074@users.noreply.github.com>
2024-03-12 11:59:28 +0000

    password genrator app in angular

--- angular_assignment/src/app/Password Generator/pass-genrator/pass-genrator.component.ts
+++ angular_assignment/src/app/Password Generator/pass-genrator/pass-genrator.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pass-genrator.component.css']
 })
 export class PassGenratorComponent implements OnInit {
-  passLength='';
+  password='';
+  passLength= 1;
+  alphabet :boolean = true;
+  num : boolean =false;
+  special :boolean =false;
   constructor() { }
 
   ngOnInit(): void {
+    this.genratePasswod();
+  }
+  genratePasswod(){
+    this.password='';
+    let str=''
+    console.log('genrating password ...');
+
+   
+    
+    if(this.alphabet){
+      str +=  'ABCDEFGHIKLMNOPQRSTVXYZabcdefghijklmnopqrstuvwxyz'
+      
+    }
+    if(this.num){
+      str +=  '1234567890';
+      
+    }
+    if (this.special) {
+      str +=  '!@#$%^&*(){}[];.,/+=-';
+    }
+
+    for (let index = 0; index < this.passLength; index++) {
+     
+      let index = Math.floor(Math.random()  * str.length)
+      this.password += str.charAt(index) 
+    }
   }
 
 }
