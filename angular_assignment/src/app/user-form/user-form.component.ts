dbf6c5582905ce6fa55f595102d3e47166966da5
Suraj Bhosale <bhosalesuraj074@gmail.com>
2024-03-13 15:15:38 +0530

    user form

--- angular_assignment/src/app/user-form/user-form.component.ts
+++ angular_assignment/src/app/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -10,6 +11,29 @@ export class UserFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  createForm(){
+        
+    user  : new  FormGroup({
+         'userDetails' : new FormGroup({
+                'fullName' : new FormControl(),
+                'birthday' : new FormControl(),
+                'gender' : new FormControl(),
+                'country' : new FormControl(),
+                'phone'  : new FormControl(),
+                'bio' : new FormControl(),
+         }),
+
+         'accountDetails' : new FormGroup({
+            'userName' : new FormControl(),
+            'email': new FormControl(),
+            'password' : new FormControl(),
+            'confirmPassword' : new FormControl(),
+         })
+
+            
+    })
   }
 
 }
