1b83593a327e11a8b053b900f05e9e499a8dfc91
Suraj Bhosale <bhosalesuraj074@gmail.com>
2024-03-15 17:35:06 +0530

    user registration form

--- angular_assignment/src/app/user-form/user-form.component.ts
+++ angular_assignment/src/app/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -20,12 +20,13 @@ export class UserFormComponent implements OnInit {
    
   }
   countries = [
+    { name: 'India', code: '+91' },
     { name: 'United States', code: '+1' },
     { name: 'Canada', code: '+1' },
     { name: 'United Kingdom', code: '+44' },
     { name: 'Germany', code: '+49' },
     { name: 'France', code: '+33' },
-    { name: 'India', code: '+91' },
+  
     // Add more countries as needed
   ];
   
@@ -37,15 +38,15 @@ export class UserFormComponent implements OnInit {
                 'birthday' : new FormControl('', [Validators.required]),
                 'gender' : new FormControl('',[Validators.required]),
                 'country' : new FormControl('', [Validators.required]),
-                'phone'  : new FormControl(),
+                'phone'  : new FormControl('', [Validators.required,this.phoneValidation]),
                 'bio' : new FormControl('', [Validators.required]),
          }),
 
          'accountDetails' : new FormGroup({
-            'userName' : new FormControl(),
-            'email': new FormControl(),
-            'password' : new FormControl(),
-            'confirmPassword' : new FormControl(),
+            'userName' : new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(25), this.validateUserName]),
+            'email': new FormControl('', [Validators.required, this.validateEmail]),
+            'password' : new FormControl('' ,[Validators.required,Validators.minLength(5), this.validatePassword]),
+            'confirmPassword' : new FormControl('', [Validators.required]),
          })
 
             
@@ -62,4 +63,77 @@ export class UserFormComponent implements OnInit {
     })
     this.setCountryCode = cc[0].code + ' - ';
   }
-}
+
+  phoneValidation(phone: FormControl): ValidationErrors | null {
+    let phoneno: string = phone.value;
+    let actual = phoneno.split(' - ');
+  
+
+    if (typeof actual[1] === 'string') {
+      // console.log('number length', actual[1].length);
+  
+      if (actual[1] == '') {
+        return { requiredNumber: true };
+      }
+  
+      if (actual[1].length+1 <= 10) {
+        return { invalidNumber: true };
+      }
+    }
+  
+    return null;
+  }
+
+ 
+  validateUserName(userName : FormControl): ValidationErrors | null{
+       let userNameFormat = /^[a-zA-Z0-9]+$/
+    if(userName.value != ''){   
+    if(!userNameFormat.test(userName.value) )
+    {
+      return { invalidUserName :true }
+    }
+  }
+    return null;
+  }
+
+  validateEmail(email: FormControl): ValidationErrors | null{
+    let emailPattern = /^[a-zA-Z][a-zA-Z0-9]*@gmail\.com$/;
+   if(email.value != ''){
+    if (!emailPattern.test(email.value)) {
+      return {invalidEmail : true}
+    }
+   }
+
+    return null;
+  }
+
+  validatePassword(password: FormControl): ValidationErrors | null{
+    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+    const alphabetRegex = /[a-zA-Z]/;
+    const numberRegex = /[0-9]/;
+
+    const hasSpecialChar = specialCharRegex.test(password.value);
+    const hasAlphabet = alphabetRegex.test(password.value);
+    const hasNumber = numberRegex.test(password.value);
+    if (password.value != ''){ 
+    if (!hasSpecialChar || !hasAlphabet || !hasNumber) {
+      return { 'invalidPasswordFormat': true };
+    }
+  }
+    return null;
+  }
+
+  
+  confirmPassword(confirmPassword: FormControl) : ValidationErrors | null
+  {
+    if(this.user != null){
+      if(this.user.get('accountDetails.password')?.value != confirmPassword.value)
+      {
+        return {invalidConfirmPassword : true};
+      }
+    }
+
+    return null
+  }
+
+}  
